fc6c201df7df734b5feaad19c9e61be4850a02fd
Developer <developer@example.com>
2026-06-01 16:30:03 +0300

    added and updated entities

---
+++ PomodoraBack/Entities/WorkspaceInvitation.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PomodoraBack.Core.Entities;
+using PomodoraBack.Core.Enums;
+
+namespace PomodoraBack.Entities;
+
+public class WorkspaceInvitation:IEntity
+{
+    [Key]
+    public string WorkspaceInvitationId { get; set; }=Guid.NewGuid().ToString();
+    public string WorkspaceId { get; set; }=string.Empty;
+    public string SenderId { get; set; } = string.Empty;
+    public string ReceiverId { get; set; } = string.Empty;
+    public WorkspaceInvitationStatusEnums Status { get; set; } = WorkspaceInvitationStatusEnums.pending; 
+    public DateTime CreatedAt { get; set; }=DateTime.Now;
+    public DateTime ExpiresAt { get; set; }
+    [ForeignKey(nameof(WorkspaceId))]
+    public Workspace Workspace { get; set; } = null!;
+    [ForeignKey(nameof(SenderId))]
+    public User Sender { get; set; } = null!;
+    [ForeignKey(nameof(ReceiverId))]
+    public User Recevier { get; set; } = null!;
+
+}
